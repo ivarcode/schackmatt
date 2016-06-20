6a9b8a2561d56472a75e61ce4f5473de552fed70
Camden Wagner <camden.i.wagner@ivarcode.net>
2016-06-20 02:47:48 -0400

    game.js & def.js

--- js/game.js
+++ js/game.js
@@ -3,5 +3,13 @@ game.js
 */
 
 function game(p1, p2, board) {
-	
+	this.p1 = p1;
+	this.p2 = p2;
+	this.board = [];
+	for (var i = 0; i < 8; i++) {
+		board[i] = [];
+		for (var j = 0; j < 8; j++) {
+			board[i][j] = BOARD_STANDARD[i][j];
+		}
+	}
 }
