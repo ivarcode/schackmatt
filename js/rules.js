3508f57ff7aeb3414fd001674510b08a6995641f
Camden Wagner <camden.i.wagner@ivarcode.net>
2016-06-22 05:01:17 -0400

    piece movement general rules

--- js/rules.js
+++ js/rules.js
@@ -5,7 +5,7 @@ rules.js
 function isLegalMove(game,src,dest) {
 	var moves = getLegalMoves(game);
 	// console.log("move " + src.x + "," + src.y + " --> " + dest.x + "," + dest.y);
-	// console.log("number of legal moves = " + moves.length);
+	console.log("number of legal moves = " + moves.length);
 	for (var i = 0; i < moves.length; i++) {
 		// console.log(moves[i].src.x + "," + moves[i].src.y + " --> " + moves[i].dest.x + "," + moves[i].dest.y);
 		if (moves[i].src.x == src.x && moves[i].src.y == src.y &&
@@ -42,8 +42,17 @@ function getMovesFromSq(sq,game) {
 	var piece = game.board[sq.x][sq.y];
 	// console.log(piece.color + " " + piece.type);
 	if (piece.type == "KING") {
-		// console.log("piece.type == KING");
 		return getKingMoves(sq,game);
+	} else if (piece.type == "QUEEN") {
+		return getQueenMoves(sq,game);
+	} else if (piece.type == "BISHOP") {
+		return getBishopMoves(sq,game);
+	} else if (piece.type == "KNIGHT") {
+		return getKnightMoves(sq,game);
+	} else if (piece.type == "ROOK") {
+		return getRookMoves(sq,game);
+	} else if (piece.type == "PAWN") {
+		return getPawnMoves(sq,game);
 	}
 	return [];
 }
@@ -66,12 +75,301 @@ function getKingMoves(sq,game) {
 		if (list[i].x > -1 && list[i].x < 8 && list[i].y > -1 && list[i].y < 8) {
 			// console.log("adding new move to the moves [] ");
 			// console.log(moves.length);
-			moves[moves.length] = {src:sq,dest:list[i],notation:null};
+			if (game.board[list[i].x][list[i].y] == null || game.board[list[i].x][list[i].y].color != game.turn) {
+				moves[moves.length] = {src:sq,dest:list[i],notation:null};
+			}
 			// console.log(moves.length);
 		}
 	}
 	// moves[moves.length] = new move(sq,sq,getNotation(sq,sq,game));
 	// console.log("king moves length = " + moves.length);
+	return moves;
+}
+
+function getKnightMoves(sq,game) {
+	var moves = [];
+	var list = [];
+	list[list.length] = {x:sq.x+1,y:sq.y+2};
+	list[list.length] = {x:sq.x+1,y:sq.y-2};
+	list[list.length] = {x:sq.x+2,y:sq.y+1};
+	list[list.length] = {x:sq.x+2,y:sq.y-1};
+	list[list.length] = {x:sq.x-1,y:sq.y+2};
+	list[list.length] = {x:sq.x-1,y:sq.y-2};
+	list[list.length] = {x:sq.x-2,y:sq.y+1};
+	list[list.length] = {x:sq.x-2,y:sq.y-1};
+	for (var i = 0; i < list.length; i++) {
+		if (list[i].x > -1 && list[i].x < 8 && list[i].y > -1 && list[i].y < 8) {
+			if (game.board[list[i].x][list[i].y] == null || game.board[list[i].x][list[i].y].color != game.turn) {
+				moves[moves.length] = {src:sq,dest:list[i],notation:null};
+			}
+		}
+	}
+	return moves;
+}
+
+function getPawnMoves(sq,game) {
+	var moves = [];
+	if (game.turn == "WHITE") {
+		if (game.board[sq.x+1][sq.y] == null) {
+			// console.log("" + sq.x + "," + sq.y + " --> " + (sq.x+1) + "," + sq.y);
+			moves[moves.length] = {src:sq,dest:{x:sq.x+1,y:sq.y},notation:null};
+		}
+		if (sq.x == 1 && game.board[sq.x+2][sq.y] == null && game.board[sq.x+1][sq.y] == null) {
+			moves[moves.length] = {src:sq,dest:{x:sq.x+2,y:sq.y},notation:null};
+		}
+		if (sq.x+1 < 8 && sq.y+1 < 8 && game.board[sq.x+1][sq.y+1] != null && game.board[sq.x+1][sq.y+1].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:sq.x+1,y:sq.y+1},notation:null};
+		}
+		if (sq.x+1 < 8 && sq.y-1 > -1 && game.board[sq.x+1][sq.y-1] != null && game.board[sq.x+1][sq.y-1].color != game.turn) {
+			// console.log("" + sq.x + "," + sq.y + " --> " + (sq.x+1) + "," + (sq.y-1));
+			moves[moves.length] = {src:sq,dest:{x:sq.x+1,y:sq.y-1},notation:null};
+		}
+	} else /*if turn == BLACK*/{
+		if (game.board[sq.x-1][sq.y] == null) {
+			moves[moves.length] = {src:sq,dest:{x:sq.x-1,y:sq.y},notation:null};
+		}
+		if (sq.x == 6 && game.board[sq.x-2][sq.y] == null && game.board[sq.x-1][sq.y] == null) {
+			moves[moves.length] = {src:sq,dest:{x:sq.x-2,y:sq.y},notation:null};
+		}
+		if (sq.x-1 > -1 && sq.y+1 < 8 && game.board[sq.x-1][sq.y+1] != null && game.board[sq.x-1][sq.y+1].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:sq.x-1,y:sq.y+1},notation:null};
+		}
+		if (sq.x-1 > -1 && sq.y-1 > -1 && game.board[sq.x-1][sq.y-1] != null && game.board[sq.x-1][sq.y-1].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:sq.x-1,y:sq.y-1},notation:null};
+		}
+	}
+	// console.log(moves.length);
+	return moves;
+}
+
+function getQueenMoves(sq,game) {
+	var moves = [];
+	var a = sq.x;
+	var b = sq.y;
+	while (a+1 < 8) {
+		a++;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a-1 > -1) {
+		a--;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (b+1 < 8) {
+		b++;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (b-1 > -1) {
+		b--;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a+1 < 8 && b+1 < 8) {
+		a++;
+		b++;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a+1 < 8 && b-1 > -1) {
+		a++;
+		b--;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a-1 > -1 && b+1 < 8) {
+		a--;
+		b++;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a-1 > -1 && b-1 > -1) {
+		a--;
+		b--;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	return moves;
+}
+
+function getRookMoves(sq,game) {
+	var moves = [];
+	var a = sq.x;
+	var b = sq.y;
+	while (a+1 < 8) {
+		a++;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a-1 > -1) {
+		a--;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (b+1 < 8) {
+		b++;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (b-1 > -1) {
+		b--;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	return moves;
+}
+
+function getBishopMoves(sq,game) {
+	var moves = [];
+	var a = sq.x;
+	var b = sq.y;
+	while (a+1 < 8 && b+1 < 8) {
+		a++;
+		b++;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a+1 < 8 && b-1 > -1) {
+		a++;
+		b--;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a-1 > -1 && b+1 < 8) {
+		a--;
+		b++;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a-1 > -1 && b-1 > -1) {
+		a--;
+		b--;
+		if (game.board[a][b] == null || game.board[a][b].color != game.turn) {
+			moves[moves.length] = {src:sq,dest:{x:a,y:b},notation:null};
+			if (game.board[a][b] != null) {
+				break;
+			}
+		} else {
+			break;
+		}
+	}
 	return moves;
 }
 
