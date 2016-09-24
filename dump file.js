f8ea2e666fa921d38389d38b43e59a2a7144cf63
Camden Wagner <camden.i.wagner@ivarcode.net>
2016-09-23 21:13:11 -0400

    progress ish

--- dump file.js
+++ dump file.js
@@ -1,12 +1,16 @@
 
 
+/*Move object constructor
+	used as the object to store all the data associated with a 'move' calculation i.e. src, dest, piece*/
+function Move(src, dest, piece) {
+	this.src = src;
+	this.dest = dest;
+	this.piece = piece;
+}
 
 
-/*Game prototype getters*/
-Game.prototype.get_FEN = function() {
-	/*returns the FEN string of the Game*/
-	return this.fen;
-};
+
+
 Game.prototype.get_king = function(color) {
 	/*finds the king of color in game*/
 	for (var i = 0; i < 8; i++) {
@@ -70,23 +74,7 @@ Game.prototype.get_piece = function(sq) {
 	/*returns the piece at sq on Game.board*/
 	return this.board[sq.x][sq.y];
 };
-Game.prototype.get_turn = function() {
-	/*returns the turn of game in the form of a string "WHITE" or "BLACK"*/
-	var i = 0;
-	while (this.get_FEN().charAt(i) != ' ') {
-		i++;
-	}
-	var turn = this.get_FEN().charAt(i+1);
-	// console.log("turn = "+turn);
-	if (turn == 'w') {
-		// console.log("get_turn WHITE");
-		return "WHITE";
-	} else if (turn == 'b') {
-		// console.log("get_turn BLACK");
-		return "BLACK";
-	}
-	return "ERR: invalid color in FEN";
-};
+
 
 
 Game.prototype.is_check = function(color) {
@@ -166,80 +154,6 @@ Game.prototype.is_sq_threatened_by = function(sq,color) {
 	return false;
 }
 
-Game.prototype.print = function(print_board) {
-	/*prints information about Game to the console and prints out char based board graphic if print_board == true*/
-	console.log("print("+print_board+")");
-	console.log("\t" + this.p1 + " vs " + this.p2);
-	console.log("\t" + this.get_turn() + " turn");
-	// console.log("\t" + this.get_legal_moves().length + " moves");
-	var n = "\t";
-	if (game.castling[0]) {
-		n += "K";
-	}
-	if (game.castling[1]) {
-		n += "Q";
-	}
-	if (game.castling[2]) {
-		n += "k";
-	}
-	if (game.castling[3]) {
-		n += "q";
-	}
-	console.log(n);
-	if (this.is_check(game.turn)) {
-		console.log("\t" + game.turn + " KING in check");
-	} else {
-		console.log("\t" + game.turn + " KING is safe");
-	}
-	this.print_PGN();
-	if (game.enPassant_allowedAt != null) {
-		console.log("\tenPassant_allowedAt " + game.enPassant_allowedAt.x + "," + game.enPassant_allowedAt.y);
-	}
-	if (print_board) {
-		var s = "";
-		for (var a = 7; a > -1; a--) {
-			s += "\t";
-			for (var b = 0; b < 8; b++) {
-				try {
-					if (this.get_piece({x:a,y:b}).color == "WHITE") {
-						if (this.get_piece({x:a,y:b}).type == "KING") {
-							s += "K";
-						} else if (this.get_piece({x:a,y:b}).type == "QUEEN") {
-							s += "Q";
-						} else if (this.get_piece({x:a,y:b}).type == "BISHOP") {
-							s += "B";
-						} else if (this.get_piece({x:a,y:b}).type == "KNIGHT") {
-							s += "N";
-						} else if (this.get_piece({x:a,y:b}).type == "ROOK") {
-							s += "R";
-						} else if (this.get_piece({x:a,y:b}).type == "PAWN") {
-							s += "P";
-						}
-					} else {
-						if (this.get_piece({x:a,y:b}).type == "KING") {
-							s += "k";
-						} else if (this.get_piece({x:a,y:b}).type == "QUEEN") {
-							s += "q";
-						} else if (this.get_piece({x:a,y:b}).type == "BISHOP") {
-							s += "b";
-						} else if (this.get_piece({x:a,y:b}).type == "KNIGHT") {
-							s += "n";
-						} else if (this.get_piece({x:a,y:b}).type == "ROOK") {
-							s += "r";
-						} else if (this.get_piece({x:a,y:b}).type == "PAWN") {
-							s += "p";
-						}
-					}
-				} catch(e) {
-					s += " ";
-					// console.log(e.message);
-				}
-			}
-			s += "\n";
-		}
-		console.log(s);
-	}
-};
 
 
 
--- js/game.js
+++ js/game.js
@@ -5,10 +5,12 @@ game.js
 /*Game object constructor
 	used as the object to store all the data associated with a created 'game' of chess*/
 function Game(p1, p2, gametype, pos, pgn) {
+	// console.log("Creating game object...");
 	this.p1 = p1;
 	this.p2 = p2;
 	this.gametype = gametype;
 	this.fen = pos;
+	// console.log("this.fen = "+this.fen);
 	this.board_from_FEN = function() {
 		/*sets the board array of the Game object to mirror the FEN*/
 		var board = [
@@ -26,7 +28,7 @@ function Game(p1, p2, gametype, pos, pgn) {
 		var b = 0;
 		var n = 1;
 		while (true) {
-			// console.log(this.get_FEN().charAt(i));
+			console.log(this.get_FEN().charAt(i));
 			if (this.get_FEN().charAt(i) == 'k') {
 				board[a][b] = bKing;
 				i++;
@@ -140,19 +142,130 @@ function Game(p1, p2, gametype, pos, pgn) {
 	};
 	this.load_data_from_FEN();
 }
-/*Move object constructor
-	used as the object to store all the data associated with a 'move' calculation i.e. src, dest, piece*/
-function Move(src, dest, piece) {
-	this.src = src;
-	this.dest = dest;
-	this.piece = piece;
-}
 /*Piece object constructor
 	used as the object to store all the data associated with a 'piece' i.e. type, color*/
 function Piece(type, color) {
 	this.type = type;
 	this.color = color;
 }
+
+
+
+/*Game prototype getters*/
+Game.prototype.get_FEN = function() {
+	/*returns the FEN string of the Game*/
+	return this.fen;
+};
+Game.prototype.get_turn = function() {
+	/*returns the turn of game in the form of a string "WHITE" or "BLACK"*/
+	var i = 0;
+	while (this.get_FEN().charAt(i) != ' ') {
+		i++;
+	}
+	var t = this.get_FEN().charAt(i+1);
+	// console.log("turn = "+turn);
+	if (t == 'w') {
+		// console.log("get_turn WHITE");
+		return "WHITE";
+	} else if (t == 'b') {
+		// console.log("get_turn BLACK");
+		return "BLACK";
+	}
+	return "ERR: invalid color in FEN";
+};
+
+
+
+
+Game.prototype.is_check = function(color) {
+	/*returns whether or not the position in game is currently check*/
+	var sq = this.get_king(color,this);
+	// console.log(sq);
+	// console.log(color);
+	if (this.is_sq_threatened_by(sq,this.get_opp_color(color))) {
+		console.log("CHECK");
+		return true;
+	}
+	return false;
+};
+
+
+
+
+Game.prototype.print = function(print_board) {
+	/*prints information about Game to the console and prints out char based board graphic if print_board == true*/
+	console.log("print("+print_board+")");
+	console.log("\t" + this.p1 + " vs " + this.p2);
+	console.log("\t" + this.get_turn() + " turn");
+	// console.log("\t" + this.get_legal_moves().length + " moves");
+	var n = "\t";
+	if (game.castling[0]) {
+		n += "K";
+	}
+	if (game.castling[1]) {
+		n += "Q";
+	}
+	if (game.castling[2]) {
+		n += "k";
+	}
+	if (game.castling[3]) {
+		n += "q";
+	}
+	console.log(n);
+	if (this.is_check(game.turn)) {
+		console.log("\t" + game.turn + " KING in check");
+	} else {
+		console.log("\t" + game.turn + " KING is safe");
+	}
+	this.print_PGN();
+	if (game.enPassant_allowedAt != null) {
+		console.log("\tenPassant_allowedAt " + game.enPassant_allowedAt.x + "," + game.enPassant_allowedAt.y);
+	}
+	if (print_board) {
+		var s = "";
+		for (var a = 7; a > -1; a--) {
+			s += "\t";
+			for (var b = 0; b < 8; b++) {
+				try {
+					if (this.get_piece({x:a,y:b}).color == "WHITE") {
+						if (this.get_piece({x:a,y:b}).type == "KING") {
+							s += "K";
+						} else if (this.get_piece({x:a,y:b}).type == "QUEEN") {
+							s += "Q";
+						} else if (this.get_piece({x:a,y:b}).type == "BISHOP") {
+							s += "B";
+						} else if (this.get_piece({x:a,y:b}).type == "KNIGHT") {
+							s += "N";
+						} else if (this.get_piece({x:a,y:b}).type == "ROOK") {
+							s += "R";
+						} else if (this.get_piece({x:a,y:b}).type == "PAWN") {
+							s += "P";
+						}
+					} else {
+						if (this.get_piece({x:a,y:b}).type == "KING") {
+							s += "k";
+						} else if (this.get_piece({x:a,y:b}).type == "QUEEN") {
+							s += "q";
+						} else if (this.get_piece({x:a,y:b}).type == "BISHOP") {
+							s += "b";
+						} else if (this.get_piece({x:a,y:b}).type == "KNIGHT") {
+							s += "n";
+						} else if (this.get_piece({x:a,y:b}).type == "ROOK") {
+							s += "r";
+						} else if (this.get_piece({x:a,y:b}).type == "PAWN") {
+							s += "p";
+						}
+					}
+				} catch(e) {
+					s += " ";
+					// console.log(e.message);
+				}
+			}
+			s += "\n";
+		}
+		console.log(s);
+	}
+};
 
 
 
