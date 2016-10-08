68c3c65c7232b3560ae84d53c3efba2238a46fce
Camden Wagner <camden.i.wagner@ivarcode.net>
2016-10-08 03:22:51 -0400

    sq_is_threatened_by function and get_positions_after_move_from_sq function

--- js/game.js
+++ js/game.js
@@ -73,9 +73,97 @@ Game.prototype.get_turn = function() {
 	}
 	return "ERR: invalid color in FEN";
 };
+Game.prototype.print = function(print_position) {
+	/*prints information about Game to the console and prints out char based board graphic if print_position == true*/
+	console.log("Game.prototype.print("+print_position+")");
+	console.log("\t" + this.p1 + " vs " + this.p2);
+	console.log("\tturn = \"" + this.get_turn()+"\"");
+	console.log("\tcurrent pos = \""+this.get_FEN()+"\"");
+	console.log("\t" + this.get_legal_positions().length + " moves");
+	var n = "";
+	if (game.castling[0]) {
+		n += "K";
+	}
+	if (game.castling[1]) {
+		n += "Q";
+	}
+	if (game.castling[2]) {
+		n += "k";
+	}
+	if (game.castling[3]) {
+		n += "q";
+	}
+	console.log("\tcastling availability :: "+n);
+	// if (this.is_check(game.turn)) {
+	// 	console.log("\t" + game.turn + " KING in check");
+	// } else {
+	// 	console.log("\t" + game.turn + " KING is safe");
+	// }
+	this.print_PGN();
+	if (game.enPassant_allowedAt != null) {
+		console.log("\tenPassant legal at " + game.enPassant_allowedAt.x + "," + game.enPassant_allowedAt.y);
+	} else {
+		console.log("\tenPassant not legal");
+	}
+	console.log("\tcurrent FEN = "+this.get_FEN());
+	if (print_position) {
+		this.print_Pos();
+	}
+};
+Game.prototype.print_Pos = function() {
+	var s = "";
+	for (var a = 7; a > -1; a--) {
+		s += "\t";
+		for (var b = 0; b < 8; b++) {
+			try {
+				if (this.get_piece({x:a,y:b}).color == "WHITE") {
+					if (this.get_piece({x:a,y:b}).type == "KING") {
+						s += "K";
+					} else if (this.get_piece({x:a,y:b}).type == "QUEEN") {
+						s += "Q";
+					} else if (this.get_piece({x:a,y:b}).type == "BISHOP") {
+						s += "B";
+					} else if (this.get_piece({x:a,y:b}).type == "KNIGHT") {
+						s += "N";
+					} else if (this.get_piece({x:a,y:b}).type == "ROOK") {
+						s += "R";
+					} else if (this.get_piece({x:a,y:b}).type == "PAWN") {
+						s += "P";
+					}
+				} else {
+					if (this.get_piece({x:a,y:b}).type == "KING") {
+						s += "k";
+					} else if (this.get_piece({x:a,y:b}).type == "QUEEN") {
+						s += "q";
+					} else if (this.get_piece({x:a,y:b}).type == "BISHOP") {
+						s += "b";
+					} else if (this.get_piece({x:a,y:b}).type == "KNIGHT") {
+						s += "n";
+					} else if (this.get_piece({x:a,y:b}).type == "ROOK") {
+						s += "r";
+					} else if (this.get_piece({x:a,y:b}).type == "PAWN") {
+						s += "p";
+					}
+				}
+			} catch(e) {
+				s += " ";
+				// console.log(e.message);
+			}
+		}
+		s += "\n";
+	}
+	console.log(s);
+};
+Game.prototype.print_PGN = function() {
+	var out = "";
+	for (var i = 0; i < this.pgn.length; i++) {
+		out += this.pgn[i].notation+" ";
+	}
+	console.log("\tPGN :: "+out);
+};
 
 
-/*Helper functions*/
+/*helper functions*/
 function board_from_FEN(fen) {
 	/*returns a board (an array containing pieces) that results from the parameter fen*/
 	var board = [
@@ -335,7 +423,20 @@ function get_positions_after_moves_from_sq(board,sq) {
 					}
 				}
 			} else /*if BLACK king on e8*/ if (source_piece_color == "BLACK" && sq.x == 7 && sq.y == 4) {
-
+				// if there is a BLACK ROOK on h8 and f8 == null and g8 == null
+				if (board[7][7].type == "ROOK" && board[7][7].color == source_piece_color && board[7][5] == null && board[7][6] == null) {
+					// KINGSIDE CASTLE
+					if (!sq_is_threatened_by(board,{x:7,y:4},get_opp_color(source_piece_color)) && !sq_is_threatened_by(board,{x:7,y:5},get_opp_color(source_piece_color)) && !sq_is_threatened_by(board,{x:7,y:6},get_opp_color(source_piece_color))) {
+						list[list.length] = {x:7,y:6};
+					}
+				}
+				// if there is a BLACK ROOK on a8 and b8 == null and c8 == null and d8 == null
+				if (board[7][0].type == "ROOK" && board[7][0].color == source_piece_color && board[7][1] == null && board[7][2] == null && board[7][3] == null) {
+					// QUEENSIDE CASTLE
+					if (!sq_is_threatened_by(board,{x:7,y:4},get_opp_color(source_piece_color)) && !sq_is_threatened_by(board,{x:7,y:3},get_opp_color(source_piece_color)) && !sq_is_threatened_by(board,{x:7,y:2},get_opp_color(source_piece_color))) {
+						list[list.length] = {x:7,y:2};
+					}
+				}
 			}
 		} else if (board[sq.x][sq.y].type == "QUEEN") {
 			var diagonals = get_diagonals_from_sq(board,sq);
@@ -443,7 +544,7 @@ function sq_is_threatened_by(board,sq,color) {
 	/*returns a boolean result if sq on board is threatened by color*/
 	var piece = null;
 	var list = [];
-	// checking adjacent sqs for king of color 'color'
+	// checking king threats
 	list[list.length] = {x:sq.x+1,y:sq.y};
 	list[list.length] = {x:sq.x+1,y:sq.y+1};
 	list[list.length] = {x:sq.x+1,y:sq.y-1};
@@ -489,104 +590,153 @@ function sq_is_threatened_by(board,sq,color) {
 			}
 		}
 	}
+	piece = null;
 	// checking flat threats
 	var a = sq.x;
 	var b = sq.y;
 	while (a+1 < 8) {
 		a++;
-		
-	}
-}
-
-
-Game.prototype.print = function(print_position) {
-	/*prints information about Game to the console and prints out char based board graphic if print_position == true*/
-	console.log("Game.prototype.print("+print_position+")");
-	console.log("\t" + this.p1 + " vs " + this.p2);
-	console.log("\tturn = \"" + this.get_turn()+"\"");
-	console.log("\tcurrent pos = \""+this.get_FEN()+"\"");
-	console.log("\t" + this.get_legal_positions().length + " moves");
-	var n = "";
-	if (game.castling[0]) {
-		n += "K";
-	}
-	if (game.castling[1]) {
-		n += "Q";
-	}
-	if (game.castling[2]) {
-		n += "k";
-	}
-	if (game.castling[3]) {
-		n += "q";
-	}
-	console.log("\tcastling availability :: "+n);
-	// if (this.is_check(game.turn)) {
-	// 	console.log("\t" + game.turn + " KING in check");
-	// } else {
-	// 	console.log("\t" + game.turn + " KING is safe");
-	// }
-	this.print_PGN();
-	if (game.enPassant_allowedAt != null) {
-		console.log("\tenPassant legal at " + game.enPassant_allowedAt.x + "," + game.enPassant_allowedAt.y);
-	} else {
-		console.log("\tenPassant not legal");
-	}
-	console.log("\tcurrent FEN = "+this.get_FEN());
-	if (print_position) {
-		this.print_Pos();
-	}
-};
-Game.prototype.print_Pos = function() {
-	var s = "";
-	for (var a = 7; a > -1; a--) {
-		s += "\t";
-		for (var b = 0; b < 8; b++) {
-			try {
-				if (this.get_piece({x:a,y:b}).color == "WHITE") {
-					if (this.get_piece({x:a,y:b}).type == "KING") {
-						s += "K";
-					} else if (this.get_piece({x:a,y:b}).type == "QUEEN") {
-						s += "Q";
-					} else if (this.get_piece({x:a,y:b}).type == "BISHOP") {
-						s += "B";
-					} else if (this.get_piece({x:a,y:b}).type == "KNIGHT") {
-						s += "N";
-					} else if (this.get_piece({x:a,y:b}).type == "ROOK") {
-						s += "R";
-					} else if (this.get_piece({x:a,y:b}).type == "PAWN") {
-						s += "P";
-					}
-				} else {
-					if (this.get_piece({x:a,y:b}).type == "KING") {
-						s += "k";
-					} else if (this.get_piece({x:a,y:b}).type == "QUEEN") {
-						s += "q";
-					} else if (this.get_piece({x:a,y:b}).type == "BISHOP") {
-						s += "b";
-					} else if (this.get_piece({x:a,y:b}).type == "KNIGHT") {
-						s += "n";
-					} else if (this.get_piece({x:a,y:b}).type == "ROOK") {
-						s += "r";
-					} else if (this.get_piece({x:a,y:b}).type == "PAWN") {
-						s += "p";
-					}
-				}
-			} catch(e) {
-				s += " ";
-				// console.log(e.message);
+		piece = board[a][b];
+		if (piece != null) {
+			if (piece.color == color && (piece.type == "ROOK" || piece.type == "QUEEN")) {
+				return true;
+			} else {
+				break;
 			}
 		}
-		s += "\n";
 	}
-	console.log(s);
-};
-Game.prototype.print_PGN = function() {
-	var out = "";
-	for (var i = 0; i < this.pgn.length; i++) {
-		out += this.pgn[i].notation+" ";
+	a = sq.x;
+	while (a-1 > -1) {
+		a--;
+		piece = board[a][b];
+		if (piece != null) {
+			if (piece.color == color && (piece.type == "ROOK" || piece.type == "QUEEN")) {
+				return true;
+			} else {
+				break;
+			}
+		}
 	}
-	console.log("\tPGN :: "+out);
-};
+	a = sq.x;
+	while (b+1 < 8) {
+		b++;
+		piece = board[a][b];
+		if (piece != null) {
+			if (piece.color == color && (piece.type == "ROOK" || piece.type == "QUEEN")) {
+				return true;
+			} else {
+				break;
+			}
+		}
+	}
+	b = sq.y;
+	while (b-1 > -1) {
+		b--;
+		piece = board[a][b];
+		if (piece != null) {
+			if (piece.color == color && (piece.type == "ROOK" || piece.type == "QUEEN")) {
+				return true;
+			} else {
+				break;
+			}
+		}
+	}
+	b = sq.y;
+	// checking diagonal threats
+	while (a+1 < 8 && b+1 < 8) {
+		a++;
+		b++;
+		piece = board[a][b];
+		if (piece != null) {
+			if (piece.color == color && (piece.type == "BISHOP" || piece.type == "QUEEN")) {
+				return true;
+			} else {
+				break;
+			}
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a+1 < 8 && b-1 > -1) {
+		a++;
+		b--;
+		piece = board[a][b];
+		if (piece != null) {
+			if (piece.color == color && (piece.type == "BISHOP" || piece.type == "QUEEN")) {
+				return true;
+			} else {
+				break;
+			}
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a-1 > -1 && b+1 < 8) {
+		a--;
+		b++;
+		piece = board[a][b];
+		if (piece != null) {
+			if (piece.color == color && (piece.type == "BISHOP" || piece.type == "QUEEN")) {
+				return true;
+			} else {
+				break;
+			}
+		}
+	}
+	a = sq.x;
+	b = sq.y;
+	while (a-1 > -1 && b-1 > -1) {
+		a--;
+		b--;
+		piece = board[a][b];
+		if (piece != null) {
+			if (piece.color == color && (piece.type == "BISHOP" || piece.type == "QUEEN")) {
+				return true;
+			} else {
+				break;
+			}
+		}
+	}
+	// checking pawn threats
+	if (color == "WHITE") {
+		try {
+			piece = board[sq.x-1][sq.y-1];
+			if (piece.color == color && piece.type == "PAWN") {
+				return true;
+			}
+		} catch(e) {
+			// console.log(e.message);
+		}
+		try {
+			piece = board[sq.x-1][sq.y+1];
+			if (piece.color == color && piece.type == "PAWN") {
+				return true;
+			}
+		} catch(e) {
+			// console.log(e.message);
+		}
+	} else if (color == "BLACK") {
+		try {
+			piece = board[sq.x+1][sq.y-1];
+			if (piece.color == color && piece.type == "PAWN") {
+				return true;
+			}
+		} catch(e) {
+			// console.log(e.message);
+		}
+		try {
+			piece = board[sq.x+1][sq.y+1];
+			if (piece.color == color && piece.type == "PAWN") {
+				return true;
+			}
+		} catch(e) {
+			// console.log(e.message);
+		}
+	}
+	// return false if none of the above conditions have led to a returning true
+	return false;
+}
+
 
 
 
