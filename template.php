4bb485490d0c0f6ce5f222e23d2441e6bd75314c
Camden Wagner <camden.i.wagner@ivarcode.net>
2016-12-12 17:53:43 -0500

    page layout

--- template.php
+++ template.php
@@ -1,3 +1,29 @@
 <!--
 template.php
 -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/layout.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+
+	<title>untitled</title>
+</head>
+<body>
+<div id="container">
+	<div id="header">
+		
+	</div>
+	<div id="navbar">
+		
+	</div>
+	<div id="content">
+		
+	</div>
+	<div id="footer">
+		
+	</div>
+</div>
+</body>
+</html>
