5939d7aae89ae7488206f5d4b4602c72e5dd9947
Camden Wagner <camden.i.wagner@ivarcode.net>
2017-02-04 22:27:26 -0500

    gotta match ya passwords

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -9,9 +9,19 @@ $name = $_POST['name'];
 $password = $_POST['password'];
 $confirm_password = $_POST['confirm_password'];
 
-$sql = "INSERT INTO user (email, username, name, password) VALUES ('$email','$username','$name','$password')";
-$result = mysqli_query($conn,$sql);
+// checking to see if password & confirm_password are equal
+if ($password == $confirm_password) {
+	// register user and return to index.php
+	$sql = "INSERT INTO user (email, username, name, password) VALUES ('$email','$username','$name','$password')";
+	$result = mysqli_query($conn,$sql);
 
-header("Location: ../index.php");
+	// redirect to index.php
+	header("Location: ../");
+} else {
+	// refresh page and throw error
+	header("Location: ../register.php?error=passwords_do_not_match");
+}
+
+
 
 ?>
--- register.php
+++ register.php
@@ -4,11 +4,21 @@ register.php
 
 <?php
 	include 'header.php';
+	include 'dbh.php';
 ?>
 
 <div id="container">
 	
 	<div id="content">
+
+		<!-- getting the url from the server to handle thrown errors -->
+		<?php
+			$url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+			// if the $url contains passwords_do_not_match error
+			if (strpos($url,'error=passwords_do_not_match') !== false) {
+				echo "<h3 class='red_error_text'>Your passwords did not match.</h3><br>";
+			}
+		?>
 		
 		<!-- php script redirects to index.php if the user is already logged in -->
 		<?php
@@ -24,7 +34,7 @@ register.php
 		<form action="includes/register.inc.php" method="POST" enctype="multipart/formdata">
 			<input id="email" type="email" name="email" placeholder="email address" required="required" autofocus><br>
 			<input id="username" type="text" name="username" placeholder="username" required="required"><br>
-			<input id="name" type="text" name="name" placeholder="name" required="required"><br>
+			<input id="name" type="text" name="name" placeholder="name (optional)"><br>
 			<input id="password" type="password" name="password" placeholder="password" required="required"><br>
 			<input id="confirm_password" type="password" name="confirm_password" placeholder="confirm password" required="required"><br>
 			<input id="register_button" type="submit" name="register" value="register">
