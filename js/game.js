1302aa37baf39e131b4956b5b8ccc6dc2e46749a
Camden Wagner <camden.i.wagner@ivarcode.net>
2017-02-14 15:59:17 -0500

    continuing code migration

--- js/game.js
+++ js/game.js
@@ -15,14 +15,50 @@ function Game(white, black, gametype, pos, pgn) {
 	this.pgn = pgn;
 }
 
-function print_game_info(g) {
-	/*prints all the information about the current state of Game 'g' into the console*/
-	console.log(g.gametype+" GAME : "+g.white+" vs "+g.black);
-	console.log("\t"+g.fen);
-	console.log("\t"+print_board_from_fen(g.fen));
-	console.log("\t"+get_turn(g.fen)+"'s turn");
-	console.log("\tavailable castles : "+get_castling_data(g.fen));
-
+function board_from_fen(fen) {
+	/*returns the board in array form from the param fen*/
+	// initializes board array
+	var board = [
+	[null,null,null,null,null,null,null,null],
+	[null,null,null,null,null,null,null,null],
+	[null,null,null,null,null,null,null,null],
+	[null,null,null,null,null,null,null,null],
+	[null,null,null,null,null,null,null,null],
+	[null,null,null,null,null,null,null,null],
+	[null,null,null,null,null,null,null,null],
+	[null,null,null,null,null,null,null,null]
+	];
+	// initializing counter vars
+	var i = 0;
+	var a = 7;
+	var b = 0;
+	while (true) {
+		if (fen.charAt(i) == 'k' || fen.charAt(i) == 'q' || fen.charAt(i) == 'b' || fen.charAt(i) == 'n' || fen.charAt(i) == 'r' || fen.charAt(i) == 'p' || fen.charAt(i) == 'K' || fen.charAt(i) == 'Q' || fen.charAt(i) == 'B' || fen.charAt(i) == 'N' || fen.charAt(i) == 'R' || fen.charAt(i) == 'P') {
+			board[a][b] = fen.charAt(i);
+			i++;
+			b++;
+		} else if (fen.charAt(i) == '/') {
+			// move down rank
+			a--;
+			i++;
+			// reset file to 0 (a)
+			b = 0;
+		} else if (fen.charAt(i) == ' ') {
+			// done filling board with pieces, break loop
+			break;
+		} else {
+			// set n to number of blank sqs
+			var n = parseInt(fen.charAt(i));
+			while (n > 0) {
+				board[a][b] = null;
+				b++;
+				// decrement n
+				n--;
+			}
+			i++;
+		}
+	}
+	return board;
 }
 function get_castling_data(fen) {
 	/*gets the sides of the board allowed to be castled on for both sides*/
@@ -46,6 +82,17 @@ function get_castling_data(fen) {
 	}
 	return data;
 }
+function get_legal_moves(fen) {
+	/*gets all the legal moves from the fen*/
+	// initializes empty moves array
+	var moves = [];
+	// translates the fen to the var board in array form
+	var board = board_from_fen(fen);
+	var turn = get_turn(fen);
+
+	
+	return moves;
+}
 function get_turn(fen) {
 	/*gets the color of the player next to move*/
 	var i = 0;
@@ -61,31 +108,6 @@ function get_turn(fen) {
 	}
 	throw "invalid color in fen";
 }
-
-
-/* PIECE */
-
-/*Piece object constructor
-used as the object to store all the data associated with a 'piece' i.e. type, color*/
-function Piece(type, color) {
-	this.type = type;
-	this.color = color;
-}
-
-
-/* MOVE */
-
-/*Move object constructor
-used as the object to store all the data associated with a 'move' calculation i.e. src, dest, piece, position*/
-function Move(src, dest, piece, position) {
-	this.src = src;
-	this.dest = dest;
-	this.piece = piece;
-	this.position = position;
-}
-
-
-
 
 function print_board_from_fen(fen) {
 	/*prints the board in the console that reflects the fen*/
@@ -107,4 +129,16 @@ function print_board_from_fen(fen) {
 	}
 	// return the board as a string
 	return string;
+}
+function print_game_info(g) {
+	/*prints all the information about the current state of Game 'g' into the console*/
+	console.log(g.gametype+" GAME : "+g.white+" vs "+g.black);
+	console.log("\t"+g.fen);
+	console.log("\t"+print_board_from_fen(g.fen));
+	console.log("\t"+get_turn(g.fen)+"'s turn");
+	console.log("\tavailable castles : "+get_castling_data(g.fen));
+	console.log("\tPGN : ");
+	var legal_moves = get_legal_moves(g.fen);
+	console.log("\t"+legal_moves);
+
 }
