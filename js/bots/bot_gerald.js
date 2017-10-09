1e5c93f42f37c985e1da8371d9a15287b436bf9e
Camden Wagner <camden.i.wagner@ivarcode.net>
2017-10-09 16:53:36 -0400

    time for to make some bots fall 2017

--- js/bots/bot_gerald.js
+++ js/bots/bot_gerald.js
@@ -2,9 +2,11 @@
 bot_gerald.js
 */
 
-function calculate_tree(game) {
+function get_best_move(game) {
 	/*function will calculate a tree of moves out of position of the input game*/
 	console.log(game.get_FEN());
 	var moves = game.get_legal_moves();
 	console.log(moves);
+
+	console.log('best move');
 }
