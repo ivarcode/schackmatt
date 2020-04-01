f1e54f543d8129d5065ea1469c4a85f318d1cba0
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-04-01 01:01:22 -0400

    reset to angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { PuzzleComponent } from './components/puzzle.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        PuzzleComponent,
-        HeaderComponent,
-        FooterComponent
-    ],
-    imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
