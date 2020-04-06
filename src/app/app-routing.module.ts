166cd64704bd1aa9f057f61f014505cd507425b4
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-04-05 23:25:18 -0400

    starting to setup game files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './components/game.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        component: GameComponent,
+        path: 'game'
+    },
+    {
+        redirectTo: 'game', // to be changed to direct to a live (or random) chess game
+        path: '**'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
