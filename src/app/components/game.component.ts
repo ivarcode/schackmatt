34b80f69f04a26f52bd1a130a7e0176987fa4667
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-04-06 03:41:53 -0400

    manipulating boardcanvas fun :D (fair warning, commit messages are gonna be garbage for a while

--- src/app/components/game.component.ts
+++ src/app/components/game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Game } from '../lib/game.library';
 
 @Component({
@@ -6,11 +6,63 @@ import { Game } from '../lib/game.library';
     templateUrl: './game.component.html',
     styleUrls: ['./game.component.css']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
     private game: Game;
+    private boardCanvas: any;
+    private boardContext: any;
+    private boardImage: any;
+    private pieceImages: any[];
 
     constructor() {
         this.game = new Game();
+
         console.log(this.game);
     }
+
+    ngOnInit() {
+        this.boardCanvas = document.getElementById('board');
+        this.boardCanvas.oncontextmenu = (events: any) => {
+            events.preventDefault();
+        };
+        this.boardContext = this.boardCanvas.getContext('2d');
+        this.pieceImages = [];
+
+        // doing pieces first
+        const pieceSources = [
+            'w_King',
+            'w_Queen',
+            'w_Bishop',
+            'w_Knight',
+            'w_Rook',
+            'w_Pawn',
+            'b_King',
+            'b_Queen',
+            'b_Bishop',
+            'b_Knight',
+            'b_Rook',
+            'b_Pawn'
+        ];
+        for (const pieceSrc of pieceSources) {
+            const pImg = new Image();
+            pImg.src = '../../assets/pieces/' + pieceSrc + '.png';
+            this.pieceImages.push(pImg);
+        }
+
+        this.boardImage = new Image();
+        this.boardImage.src = '../../assets/board_640x640.png';
+        // because apparently I have to wait on the image smh
+        this.boardImage.onload = () => {
+            this.drawBoard();
+        };
+    }
+
+    drawBoard(): void {
+        this.boardContext.restore();
+        this.boardContext.globalAlpha = 1;
+        // this.boardContext.fillStyle = 'yellow';
+        // this.boardContext.fillRect(0, 0, 40, 40);
+        this.boardContext.drawImage(this.boardImage, 0, 0);
+    }
+
+    refreshCanvasSquare(x: number, y: number): void {}
 }
