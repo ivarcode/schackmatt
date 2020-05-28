ca139b1c0636c9e9a9169766445d642226545473
N-Holm <57479976+N-Holm@users.noreply.github.com>
2020-05-28 03:01:05 -0400

    tactics trainer and repertoire builder buttons working

---
+++ src/app/components/tactics-trainer/tactics-trainer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-tactics-trainer',
+    templateUrl: './tactics-trainer.component.html',
+    styleUrls: ['./tactics-trainer.component.css']
+})
+export class TacticsTrainer {
+    constructor() { }
+}
