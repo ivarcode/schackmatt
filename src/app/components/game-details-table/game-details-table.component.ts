4ee840dd73070ebb3caeb6e58488a005540c91c6
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-05-30 07:11:11 -0400

    yay i know how to tie things together

--- src/app/components/game-details-table/game-details-table.component.ts
+++ src/app/components/game-details-table/game-details-table.component.ts
@@ -1,16 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
 
 @Component({
     selector: 'app-game-details-table',
     templateUrl: './game-details-table.component.html',
     styleUrls: ['./game-details-table.component.css']
 })
-export class GameDetailTableComponent {
+export class GameDetailTableComponent implements OnChanges {
     @Input() pgn: string;
+    private pgnArray: any[];
 
-    constructor() {}
+    constructor() {
+        this.pgnArray = [];
+    }
 
-    onInit(): void {}
+    // ngOnInit() {}
 
-    change(): void {}
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('changes', changes);
+
+        // TODO check if pgn changed
+        this.constructPGNArray();
+        console.log(this.pgnArray);
+    }
+
+    private constructPGNArray(): void {
+        this.pgnArray = [];
+        const split = this.pgn.split(' ');
+        // console.log('split', split);
+        for (let i = 1; i < split.length; i++) {
+            let n = null;
+            let w = null;
+            let b = null;
+            if (i < split.length) {
+                n = split[i];
+            }
+            if (i + 1 < split.length) {
+                w = split[i + 1];
+            }
+            if (i + 2 < split.length) {
+                b = split[i + 2];
+            }
+            if (n !== null || n !== undefined) {
+                this.pgnArray.push({
+                    number: n,
+                    white: w,
+                    black: b
+                });
+            }
+            i += 2;
+        }
+    }
+
+    public getPGNArray(): any[] {
+        return this.pgnArray;
+    }
 }
