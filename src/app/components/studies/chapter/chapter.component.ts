50a329d6e9bfd0f55788438bfff4b494b84f9add
N-Holm <57479976+N-Holm@users.noreply.github.com>
2020-06-01 13:11:32 -0400

    created a basic chapter object that will hold multiple pages once fully finished rn it has a create page button with the book thumbnail image as a placeholder for what will be the page image

---
+++ src/app/components/studies/chapter/chapter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-chapter',
+    templateUrl: './chapter.component.html',
+    styleUrls: ['./chapter.component.css']
+})
+export class ChapterComponent {
+    constructor() {}
+}
