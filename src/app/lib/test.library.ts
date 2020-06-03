8b17d0e7b7d1cbf4ff1cfdfa18d1924af2db171f
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-06-02 23:34:05 -0400

    progress, going to cause more trouble tomorrow

--- src/app/lib/test.library.ts
+++ src/app/lib/test.library.ts
@@ -10,31 +10,77 @@ export class Opening {
     data: Branch;
     constructor(pgn: string) {
         this.data = this.parsePGN(
+            this.data,
             'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
             pgn
         );
+        console.log(this.getJSONTree(this.data, 1));
     }
-    private parsePGN(initPosition: string, pgn: string): Branch {
+    private parsePGN(root: Branch, initPosition: string, pgn: string): Branch {
         console.log('called');
         let position = initPosition;
         let game = new Game(position);
-        let root: Branch;
+        root = { definingMove: null, fen: initPosition, options: [] };
+        let currNode = root;
         let i = 0;
-        let HARDSTOP = 0;
-        while (i <= pgn.length && HARDSTOP < 200) {
+        while (i <= pgn.length) {
             let j = i;
-            // console.log(pgn.charAt(j));
-            while (pgn.charAt(j) !== ' ' && j < 25) {
-                console.log('iter');
+            console.log(pgn.charAt(j));
+            while (pgn.charAt(j) !== ' ' && j < pgn.length) {
+                // console.log('iter');
                 j++;
-                HARDSTOP++;
             }
-            console.log(pgn.substr(i, j - i));
-            i++;
-            HARDSTOP++;
+            let a = pgn.substr(i, j - i);
+            console.log('[' + a + ']');
+            // if 1-9
+            if (a.charCodeAt(0) <= 57 && a.charCodeAt(0) >= 49) {
+                // just a number, ignore
+            } else if (a.charCodeAt(0) === 40) {
+                // (
+                console.log('i is curr', pgn.charAt(i));
+                let k = i + 1;
+                while (k < pgn.length) {
+                    console.log('entering garbage');
+                    if (pgn.charCodeAt(k) === 41) {
+                        // )
+                        currNode.options.push(
+                            this.parsePGN(
+                                currNode,
+                                null,
+                                pgn.substr(i + 1, k - i)
+                            )
+                        );
+                        break;
+                    }
+                    k++;
+                }
+            } else {
+                // add as move
+                currNode.definingMove = a;
+                currNode.options.push({
+                    definingMove: null,
+                    fen: null,
+                    options: []
+                });
+                currNode = currNode.options[0];
+            }
+            i = j + 1;
         }
-        console.log('done');
+        console.log('done', root);
+        // console.log('JSON', JSON.stringify(root));
         return root;
+    }
+    private getJSONTree(b: Branch, spaces: number): string {
+        let s = '';
+        s += b.definingMove;
+        for (let opt of b.options) {
+            s += '\n';
+            for (let i = 0; i < spaces; i++) {
+                s += ' ';
+            }
+            s += this.getJSONTree(opt, spaces + 1);
+        }
+        return s;
     }
 }
 
