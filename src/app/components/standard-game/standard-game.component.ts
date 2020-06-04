9b9702cfab2fd3fddbb4c976a6315feb9db519d2
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-06-04 01:05:53 -0400

    got a move tree working completely, i think

--- src/app/components/standard-game/standard-game.component.ts
+++ src/app/components/standard-game/standard-game.component.ts
@@ -15,7 +15,9 @@ export class StandardGameComponent implements OnInit {
     }
     ngOnInit() {
         let opening = new Opening(
-            '1. e4 e5 2. Nf3 Nc6 3. c3 Nf6 (3... d5 4. Qa4) 4. d4 *'
+            // '1. e4 e5 2. Nf3 Nc6 3. c3 Nf6 (3... d5 4. Qa4) 4. d4 *'
+            '1. e4 d5 2. exd5 Qxd5 3. Nc3 Qd8 4. Nf3 Bg4 5. d4 (5. Bc4 Qxd2+ 6. Bxd2 Nc6 (6... Bxf3 7. Qxf3 e5 8. O-O-O Bb4) 7. O-O O-O-O) 5... e5 6. dxe5 f6 7. exf6 Nxf6 *'
+            // '1. e4 d5 (1... e5)  (1... f5)  (1... c5)  (1... b5) *'
         );
         console.log('print da opening in JSON here ');
     }
