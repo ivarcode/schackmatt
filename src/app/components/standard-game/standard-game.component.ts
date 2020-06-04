1fbe8656fe3750a1e88889dab39156f273f05d96
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-06-04 01:09:12 -0400

    renaming opening library

--- src/app/components/standard-game/standard-game.component.ts
+++ src/app/components/standard-game/standard-game.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/app/lib/game.library';
-import { Opening } from 'src/app/lib/test.library';
+import { Opening } from 'src/app/lib/opening.library';
 
 @Component({
     selector: 'app-standard-game',
