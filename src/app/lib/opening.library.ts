666bd69d70e5a979601ff2ee88e2c8b2dd9e8a4f
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-06-04 21:57:05 -0400

    working on keeping a pos hist log and getting through full opening tree from lichess export format

--- src/app/lib/opening.library.ts
+++ src/app/lib/opening.library.ts
@@ -12,19 +12,18 @@ export class Opening {
             pgn
         );
         console.log('--------');
+        console.log('data', this.data);
         console.log(this.getJSONTree(this.data, 1));
     }
     private parsePGN(root: Branch, initPosition: string, pgn: string): Branch {
-        console.log('called with ', pgn);
-        let position = initPosition;
-        let game = new Game(position);
+        // console.log('called with ', pgn);
+        let game = new Game(initPosition);
+        let positionHistArray = [initPosition];
         let currNode = root;
         let i = 0;
         while (i <= pgn.length) {
-            // console.log('currnode', currNode);
             let j = i;
             while (pgn.charAt(j) !== ' ' && j < pgn.length) {
-                // console.log('iter');
                 j++;
             }
             let a = pgn.substr(i, j - i);
@@ -37,12 +36,11 @@ export class Opening {
             ) {
                 // just a number or empty space or is EMPTY string, ignore
                 i = j + 1;
-                console.log('skip');
+                // console.log('skip');
             } else if (a === '*') {
                 i = j + 1;
-                // console.log('* indicating end character');
             } else if (a.charCodeAt(0) === 40) {
-                console.log('alt line');
+                // console.log('alt line');
                 // (
                 let k = i + 1;
                 let count = 1;
@@ -53,10 +51,13 @@ export class Opening {
                         if (pgn.charCodeAt(k) === 41) {
                             if (count === 1) {
                                 // )
+                                console.log('poshist', positionHistArray);
                                 currNode.options.push(
                                     this.parsePGN(
                                         null,
-                                        null,
+                                        positionHistArray[
+                                            positionHistArray.length - 2
+                                        ],
                                         pgn.substr(i + 1, k - i)
                                     )
                                 );
@@ -71,19 +72,22 @@ export class Opening {
                 i = k + 2;
                 // console.log('i is at ', pgn.substr(i, 5));
             } else {
-                console.log('normal move');
+                // console.log('normal move');
                 // trimming front and back of ( or )
                 a.charCodeAt(0) === 40 ? (a = a.substr(1)) : (a = a);
                 a.charCodeAt(a.length - 1) === 41
                     ? (a = a.substr(0, a.length - 1))
                     : (a = a);
+                // console.log('a', a);
+                console.log('game', game.getFEN(), game, game.getLegalMoves());
+                game.makeMove(a);
+                positionHistArray.push(game.getFEN());
                 const nextNode = {
                     definingMove: a,
-                    fen: null,
+                    fen: positionHistArray[positionHistArray.length - 1],
                     options: []
                 };
                 if (currNode === null) {
-                    console.log('A');
                     currNode = nextNode;
                     if (root === null) {
                         root = currNode;
@@ -93,14 +97,12 @@ export class Opening {
                         // traverse if necessary
                         currNode = currNode.options[0];
                     }
-                    console.log('B');
                     // add as move
                     currNode.options.push(nextNode);
                 }
                 i = j + 1;
             }
         }
-        console.log('done', root);
         // console.log('JSON', JSON.stringify(root));
         return root;
     }
