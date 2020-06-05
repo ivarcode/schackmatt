5b86ea395315dc063e5e9547027eb3f3bf1b87bb
N-Holm <57479976+N-Holm@users.noreply.github.com>
2020-06-05 00:13:15 -0400

    more work on page component

--- src/app/components/studies/page/page.component.ts
+++ src/app/components/studies/page/page.component.ts
@@ -1,10 +1,22 @@
 import { Component, Input } from '@angular/core';
-import { GameComponent } from 'src/app/components/game/game.component';
+import { Game } from 'src/app/lib/game.library';
+
 @Component({
     selector: 'app-page',
     templateUrl: './page.component.html',
     styleUrls: ['./page.component.css']
 })
 export class PageComponent {
-    constructor() {}
+    private game: Game;
+    private title: string;
+    constructor() {
+        this.title = 'Working Title';
+        this.game = new Game();
+    }
+    public getGame(): Game {
+        return this.game;
+    }
+    public navigationDataEvent(data: string): void {
+        console.log('nav data event function', data);
+    }
 }
