bad0371fd2ebc8a08e07357852ab6263a5d7dd70
N-Holm <57479976+N-Holm@users.noreply.github.com>
2020-06-20 15:40:12 -0400

    Study stuff and some move nonsense

--- src/app/components/studies/book-thumbnail/book-thumbnail.component.ts
+++ src/app/components/studies/book-thumbnail/book-thumbnail.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges } from "@angular/core";
 
 @Component({
-    selector: 'app-book-thumbnail',
-    templateUrl: './book-thumbnail.component.html',
-    styleUrls: ['./book-thumbnail.component.css']
+  selector: "app-book-thumbnail",
+  templateUrl: "./book-thumbnail.component.html",
+  styleUrls: ["./book-thumbnail.component.css"],
 })
 export class BookThumbnailComponent implements OnChanges {
-    @Input() fen: string;
-    constructor() {}
-    ngOnChanges(changes: SimpleChanges) {
-        // this will fire when changes
-    }
+  @Input() fen: string;
+  constructor() {}
+  ngOnChanges(changes: SimpleChanges) {
+    // this will fire when changes
+  }
 }
