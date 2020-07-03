092dd8e8cf9d13ce8da13bce3387778f0f487e47
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-07-02 22:20:33 -0400

    board overlay component, apparently bootstrap 4.5 works when it isnt upgraded? and working towards a spinner loader

---
+++ src/app/components/board-overlay/board-overlay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-board-overlay',
+    templateUrl: './board-overlay.component.html',
+    styleUrls: ['./board-overlay.component.css']
+})
+export class BoardOverlayComponent {
+    private title: string;
+    constructor() {}
+}
