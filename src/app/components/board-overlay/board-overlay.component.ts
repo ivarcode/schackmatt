8f2acb4f6397ec82d39fc539856b26d9703fceec
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-07-03 21:11:21 -0400

    more ropes

--- src/app/components/board-overlay/board-overlay.component.ts
+++ src/app/components/board-overlay/board-overlay.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
 })
 export class BoardOverlayComponent {
     private title: string;
-    constructor() {}
+    private displayLoadingMessage: boolean;
+    constructor() {
+        this.displayLoadingMessage = false;
+    }
+
+    public getDisplayLoadingMessage(): boolean {
+        return this.displayLoadingMessage;
+    }
 }
