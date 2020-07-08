1b0d933586924a26aa674fb03ca8cdf8c7b0b235
N-Holm <57479976+N-Holm@users.noreply.github.com>
2020-07-08 04:56:38 -0400

    working on it still

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
     styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-    constructor() {}
+    public staffMembers: {
+        name: string;
+        title: string;
+        bio: string;
+        discord: string;
+        url: string;
+    }[];
+    constructor() {
+        this.staffMembers = [
+            {
+                name: 'Camden Wagner',
+                title: 'Head Developer',
+                bio: 'chess player',
+                discord: 'ivarcode',
+                url: '../../../assets/Cam.png'
+            },
+            {
+                name: 'Nick Groh',
+                title: 'Head of Marketing',
+                bio: 'chess player',
+                discord: 'Ragchess',
+                url: '../../../assets/Nick.png'
+            },
+            {
+                name: 'Nicolas Holmgren',
+                title: 'Junior Developer',
+                bio: 'chess player',
+                discord: 'Enrico',
+                url: '../../../assets/Nick.png'
+            }
+        ];
+    }
 }
