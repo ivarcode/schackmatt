723029eba2214fa103b2bb0a2f56c19838f867bb
Camden Wagner <camden.i.wagner@ivarcode.net>
2021-01-20 00:00:06 -0500

    preparing httpclient testing endpoint data

--- src/app/services/lichess-study.service.ts
+++ src/app/services/lichess-study.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
     providedIn: 'root'
 })
+// LichessAPIService
 export class LichessStudyService {
     constructor() {
         console.log('lichess study service');
