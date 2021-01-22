8f8ba159e8da61f4b3599ce6e5420b7203b7a5fa
Camden Wagner <camden.i.wagner@ivarcode.net>
2021-01-21 19:15:25 -0500

    leaving off here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -44,7 +45,7 @@ import { LichessStudyService } from './services/lichess-study.service';
         BoardOverlayComponent,
         EndgameTrainerComponent
     ],
-    imports: [BrowserModule, AppRoutingModule, FormsModule],
+    imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
     providers: [LichessStudyService],
     bootstrap: [AppComponent]
 })
--- src/app/services/lichess-study.service.ts
+++ src/app/services/lichess-study.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,15 @@ import { Injectable } from '@angular/core';
 })
 // LichessAPIService
 export class LichessStudyService {
-    constructor() {
+    constructor(private http: HttpClient) {
         console.log('lichess study service');
+
+        this.getStudy().subscribe((data: any) => {
+            console.log('study', data);
+        });
+    }
+
+    private getStudy() {
+        return this.http.get('https://lichess.org/SOMEURL');
     }
 }
