eb233a17f2602bbdc70c8aa963e5a3c708926d98
Camden Wagner <camden.i.wagner@ivarcode.net>
2021-01-24 20:11:06 -0500

    python cutting up file tests

--- parseFile.py
+++ parseFile.py
@@ -4,8 +4,18 @@ import sys
 
 if len(sys.argv) != 2:
     print('Err: 1 argument required (target filename)')
+    exit()
 else:
     print(sys.argv[1])
+
+f = open(sys.argv[1], 'r')
+# print(f.read())
+
+obj = {}
+
+for line in f:
+    if line[0] == '[':
+        print(line)
 
 # for arg in sys.argv:
 
