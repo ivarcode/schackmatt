20bdfe3657a417eb0084751022a640d104b41a6b
Camden Wagner <camden.i.wagner@ivarcode.net>
2021-02-27 09:57:49 -0500

    heavy reorganization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AccountComponent } from './components/account/account.component';
-import { TacticsTrainerComponent } from './components/tactics-trainer/tactics-trainer.component';
-import { RepertoireBuilderComponent } from './components/repertoire-builder/repertoire-builder.component';
-import { MyBooksComponent } from './components/studies/my-books/my-books.component';
-import { PublicBooksComponent } from './components/studies/public-books/public-books.component';
-import { BookComponent } from './components/studies/book/book.component';
-import { ChapterComponent } from './components/studies/chapter/chapter.component';
+import { PuzzlesComponent } from './components/puzzles/puzzles.component';
 import { OpeningTrainingGameComponent } from './components/opening-training-game/opening-training-game.component';
-import { PageComponent } from './components/studies/page/page.component';
 import { AboutComponent } from './components/about/about.component';
 import { EndgameTrainerComponent } from './components/endgame-trainer/endgame-trainer.component';
 
 const routes: Routes = [
+    {
+        component: PuzzlesComponent,
+        path: 'puzzles'
+    },
     {
         component: EndgameTrainerComponent,
         path: 'endgame-trainer'
@@ -22,49 +19,19 @@ const routes: Routes = [
         path: 'opening-training-game'
     },
     {
-        component: AccountComponent,
-        path: 'account'
-    },
-    {
-        component: TacticsTrainerComponent,
-        path: 'tactics-trainer'
-    },
-    {
-        component: RepertoireBuilderComponent,
-        path: 'repertoire-builder'
-    },
-    {
-        component: MyBooksComponent,
-        path: 'my-books'
-    },
-    {
-        component: PublicBooksComponent,
-        path: 'public-books'
-    },
-    {
-        component: BookComponent,
-        path: 'book'
-    },
-    {
-        component: ChapterComponent,
-        path: 'chapter'
-    },
-    {
-        component: PageComponent,
-        path: 'page'
-    },
-    {
         component: AboutComponent,
         path: 'about'
     },
     {
-        redirectTo: 'endgame-trainer',
+        redirectTo: 'puzzles',
         path: '**'
     }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    imports: [
+        RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+    ],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,9 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { GameComponent } from './components/game/game.component';
-import { AccountComponent } from './components/account/account.component';
-import { TacticsTrainerComponent } from './components/tactics-trainer/tactics-trainer.component';
-import { RepertoireBuilderComponent } from './components/repertoire-builder/repertoire-builder.component';
-import { MyBooksComponent } from './components/studies/my-books/my-books.component';
-import { PublicBooksComponent } from './components/studies/public-books/public-books.component';
+import { PuzzlesComponent } from './components/puzzles/puzzles.component';
 import { GameDetailTableComponent } from './components/game-details-table/game-details-table.component';
-import { BookThumbnailComponent } from './components/studies/book-thumbnail/book-thumbnail.component';
-import { BookComponent } from './components/studies/book/book.component';
-import { ChapterComponent } from './components/studies/chapter/chapter.component';
 import { OpeningTrainingGameComponent } from './components/opening-training-game/opening-training-game.component';
-import { PageComponent } from './components/studies/page/page.component';
 import { AboutComponent } from './components/about/about.component';
 import { BoardOverlayComponent } from './components/board-overlay/board-overlay.component';
 import { EndgameTrainerComponent } from './components/endgame-trainer/endgame-trainer.component';
@@ -28,20 +20,12 @@ import { EndgameTrainerComponent } from './components/endgame-trainer/endgame-tr
         HeaderComponent,
         FooterComponent,
         GameComponent,
-        AccountComponent,
-        TacticsTrainerComponent,
-        RepertoireBuilderComponent,
-        MyBooksComponent,
-        PublicBooksComponent,
         GameDetailTableComponent,
-        BookThumbnailComponent,
-        BookComponent,
-        ChapterComponent,
         OpeningTrainingGameComponent,
-        PageComponent,
         AboutComponent,
         BoardOverlayComponent,
-        EndgameTrainerComponent
+        EndgameTrainerComponent,
+        PuzzlesComponent
     ],
     imports: [BrowserModule, AppRoutingModule, FormsModule],
     providers: [],
