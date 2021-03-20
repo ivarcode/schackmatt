e7ad968ce7ac2f5a9739e5404d371f4dc5002b03
Camden Wagner <camden.i.wagner@ivarcode.net>
2021-03-20 17:09:13 -0400

    dev-deploy script

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -1,4 +1,12 @@
 sshKey="./sshkeys/cwagner0"
-devIP=
-echo 
-ssh -i $sshKey $devIP "sudo rm -rf /usr/share/nginx/schackmatt/*
+sed -n 2p conf/deploymentConfig.json > tempFile.md
+sed -i 's/    \"devServerIP\": \"//g' tempFile.md
+sed -i 's/\",//g' tempFile.md
+devIP=`cat tempFile.md`
+echo "Reading dev server IP address from deploymentConfig.json..."
+echo "  :" $devIP
+rm tempFile.md
+echo "Attempting to remove files from nginx..."
+ssh -i $sshKey $devIP "sudo rm -rf /usr/share/nginx/schackmatt/*"
+echo "Attempting to copy files from /dist to nginx..."
+scp -r -i $sshKey ./dist/schackmatt/ $devIP:/usr/share/nginx
