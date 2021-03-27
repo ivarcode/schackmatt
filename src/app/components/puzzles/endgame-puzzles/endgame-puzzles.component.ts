b7c732aae2252b70ae4f8fe5f5a2131f1375377e
Camden Wagner <camden.i.wagner@ivarcode.net>
2021-03-27 18:03:36 -0400

    playing with routerLinks

--- src/app/components/puzzles/endgame-puzzles/endgame-puzzles.component.ts
+++ src/app/components/puzzles/endgame-puzzles/endgame-puzzles.component.ts
@@ -1,24 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { endgame_checkmates } from 'data/endgame_checkmates.json';
 import { queen_endgames } from 'data/queen_endgames.json';
 import { pawn_endgames } from 'data/pawn_endgames.json';
 import { Puzzle } from 'src/app/lib/interface.library';
 import { parseLichessStudies } from 'src/app/lib/util.library';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-endgame-puzzles',
     templateUrl: './endgame-puzzles.component.html',
     styleUrls: ['./endgame-puzzles.component.css']
 })
-export class EndgamePuzzlesComponent {
+export class EndgamePuzzlesComponent implements OnInit {
     private _endgamePuzzles: Puzzle[];
-    constructor() {
+    private _endgameSet: string;
+    private studyObjects = {
+        endgame_checkmates,
+        queen_endgames,
+        pawn_endgames
+    };
+    constructor(private route: ActivatedRoute) {
         this.endgamePuzzles = [
-            ...parseLichessStudies(endgame_checkmates),
-            ...parseLichessStudies(queen_endgames),
-            ...parseLichessStudies(pawn_endgames)
+            // ...parseLichessStudies(endgame_checkmates),
+            // ...parseLichessStudies(queen_endgames),
+            // ...parseLichessStudies(pawn_endgames)
         ];
+
         // console.log('endgamepuzzles', this.endgamePuzzles);
+    }
+
+    ngOnInit() {
+        this.route.queryParams.subscribe((params) => {
+            this.endgameSet = params.set;
+            this.endgamePuzzles = parseLichessStudies(
+                this.studyObjects[this.endgameSet]
+            );
+        });
+    }
+
+    get endgameSet(): string {
+        return this._endgameSet;
+    }
+    set endgameSet(s: string) {
+        this._endgameSet = s;
     }
     get endgamePuzzles(): Puzzle[] {
         return this._endgamePuzzles;
