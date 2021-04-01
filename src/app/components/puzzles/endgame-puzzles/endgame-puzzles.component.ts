aa20b7f8b03c75551e7f1497a86c54d0c39207be
Camden Wagner <camden.i.wagner@ivarcode.net>
2021-04-01 00:22:10 -0400

    finally getting the queryParams working how they kinda should be

--- src/app/components/puzzles/endgame-puzzles/endgame-puzzles.component.ts
+++ src/app/components/puzzles/endgame-puzzles/endgame-puzzles.component.ts
@@ -4,7 +4,7 @@ import { queen_endgames } from 'data/queen_endgames.json';
 import { pawn_endgames } from 'data/pawn_endgames.json';
 import { Puzzle } from 'src/app/lib/interface.library';
 import { parseLichessStudies } from 'src/app/lib/util.library';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     selector: 'app-endgame-puzzles',
@@ -13,13 +13,14 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EndgamePuzzlesComponent implements OnInit {
     private _endgamePuzzles: Puzzle[];
-    private _endgameSet: string;
     private studyObjects = {
         endgame_checkmates,
         queen_endgames,
         pawn_endgames
     };
-    constructor(private route: ActivatedRoute) {
+    // public endgamePuzzleParamObject: { set: 'pawn_endgames' };
+
+    constructor(private route: ActivatedRoute, private router: Router) {
         this.endgamePuzzles = [
             // ...parseLichessStudies(endgame_checkmates),
             // ...parseLichessStudies(queen_endgames),
@@ -31,19 +32,22 @@ export class EndgamePuzzlesComponent implements OnInit {
 
     ngOnInit() {
         this.route.queryParams.subscribe((params) => {
-            this.endgameSet = params.set;
+            const defaultParamString = 'endgame_checkmates';
+            let paramString: string;
+            if (!params?.set) {
+                this.router.navigate(['endgame_puzzles'], {
+                    queryParams: { set: defaultParamString }
+                });
+                paramString = defaultParamString;
+            } else {
+                paramString = params.set;
+            }
             this.endgamePuzzles = parseLichessStudies(
-                this.studyObjects[this.endgameSet]
+                this.studyObjects[paramString]
             );
         });
     }
 
-    get endgameSet(): string {
-        return this._endgameSet;
-    }
-    set endgameSet(s: string) {
-        this._endgameSet = s;
-    }
     get endgamePuzzles(): Puzzle[] {
         return this._endgamePuzzles;
     }
